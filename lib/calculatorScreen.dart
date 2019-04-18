29647e1ba76a2d36e50737b1cf887d9eb7656730
Ian Leshan <ianleshan71@gmail.com>
2019-04-18 14:39:01 -0400

    Numberbutton

--- lib/calculatorScreen.dart
+++ lib/calculatorScreen.dart
@@ -23,78 +23,10 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
           Expanded(
             child: Row(
               children: <Widget>[
-                Expanded(
-                  child: Container(
-                    child: Center(
-                      child: Text(
-                        "AC",
-                        style: TextStyle(
-                          fontSize: 50.0,
-                          fontWeight: FontWeight.w300,
-                          color: Colors.black,
-                        ),
-                      ),
-                    ),
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.grey,
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Container(
-                    child: Center(
-                      child: Text(
-                        "+/-",
-                        style: TextStyle(
-                          fontSize: 50.0,
-                          fontWeight: FontWeight.w300,
-                          color: Colors.black,
-                        ),
-                      ),
-                    ),
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.grey,
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Container(
-                    child: Center(
-                      child: Text(
-                        "%",
-                        style: TextStyle(
-                          fontSize: 50.0,
-                          fontWeight: FontWeight.w300,
-                          color: Colors.black,
-                        ),
-                      ),
-                    ),
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.grey,
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Container(
-                    child: Center(
-                      child: Text(
-                        "÷",
-                        style: TextStyle(
-                          fontSize: 50.0,
-                          fontWeight: FontWeight.w300,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.orange,
-                    ),
-                  ),
-                ),
+                NumberButton("AC", Colors.grey, Colors.black),
+                NumberButton("+/-", Colors.grey, Colors.black),
+                NumberButton("%", Colors.grey, Colors.black),
+                NumberButton("÷", Colors.orange, Colors.white),
               ],
             ),
           ),
@@ -411,6 +343,36 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class NumberButton extends StatelessWidget {
+  String text;
+  Color color;
+  Color textColor;
+
+  NumberButton(this.text, this.color, this.textColor);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        child: Center(
+          child: Text(
+            text,
+            style: TextStyle(
+              fontSize: 50.0,
+              fontWeight: FontWeight.w300,
+              color: textColor,
+            ),
+          ),
+        ),
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: color,
+        ),
       ),
     );
   }
